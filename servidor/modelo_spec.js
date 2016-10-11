94bfa8f422362af580fe1f46bfe6d0a971cdb4cf
jgallud <jgallud@gmail.com>
2016-10-11 09:26:40 +0200

    test de obtenerUsuario

--- servidor/modelo_spec.js
+++ servidor/modelo_spec.js
@@ -19,4 +19,17 @@ describe("El juego niveles incialmente...",function(){
 		expect(juego.usuarios[0]).toEqual(usuario);
 		expect(juego.usuarios[0].nombre).toEqual("Pepe");
 	});
+
+	it("comprobar obtenerUsuario (usuario existente)",function(){
+		var usuario=new modelo.Usuario("Pepe");
+		juego.agregarUsuario(usuario);		
+		var id=usuario.id;
+		var usu=juego.obtenerUsuario(id);
+		expect(usu.id).toEqual(id);
+	});
+	it("comprobar obtenerUsuario (usuario no existente",function(){
+		var id=00001;
+		var usuario=juego.obtenerUsuario(id);
+		expect(usuario).toBeUndefined();
+	});
 })
